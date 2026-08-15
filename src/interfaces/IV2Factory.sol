6e059c6246d12b37ec8f4bd87e80f8d9dbeb9a9e
Samoolino <samo4au@gmail.com>
2026-08-15 08:45:28 +0100

    style: align factory interface with Foundry formatter

--- src/interfaces/IV2Factory.sol
+++ src/interfaces/IV2Factory.sol
@@ -2,5 +2,8 @@
 pragma solidity ^0.8.26;
 
 interface IV2Factory {
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function getPair(
+        address tokenA,
+        address tokenB
+    ) external view returns (address pair);
 }
